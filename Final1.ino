d5ccd532634e0a12b08c1ffb4d969da86917e97c
Khang25 <145546028+Khang25@users.noreply.github.com>
2024-07-20 20:06:37 +0700

    Add files via upload

--- Final1.ino
+++ Final1.ino
@@ -3,31 +3,29 @@
 
 // Define Pin for motor
 const int stepPinX = 2;
-const int dirPinX = 3;
+const int dirPinX = 6;
 
-const int stepPinY1 = 4;
-const int dirPinY1 = 5;
-const int stepPinY2 = 6;
-const int dirPinY2 = 7;
+const int stepPinY1 = 3;
+const int dirPinY1 = 7;
+const int stepPinY2 = 4;
+const int dirPinY2 = 8;
 
-const int stepPinZ = 8;
+const int stepPinZ = 5;
 const int dirPinZ = 9;
-
 
 // Define Pin for Limit Switch
 const int limitSwitchX = 10;
 const int limitSwitchY1 = 11;
 const int limitSwitchY2 = 12;
 
-int Accel_X = 300;
-int Accel_Y = 300;
+// Define Emergency Button
+const int EM_Button = 18;
+volatile bool EM_Button_Pressed = false; 
 
-
-int High_Step = 0;
-int common_Step = 0;
+// Define Buzzor
+const int Buzzor = 17;
 
 char Serial_Read();
-void Reset_All();
 void Move_by_Distance(float Distance[]);
 
 int dirPin[4] = {dirPinX, dirPinY1, dirPinY2, dirPinZ};
@@ -39,8 +37,6 @@ AccelStepper y2_axis(AccelStepper::DRIVER, stepPin[2], dirPin[2]);
 AccelStepper z_axis(AccelStepper::DRIVER, stepPin[3], dirPin[3]);
 MultiStepper steppers;
 
-
-float Dist_To_Move[3];
 long Stp_To_Move[4];
 unsigned int M_Direction[3] = {0, 0, 0}; // moving direction
 
@@ -57,25 +53,39 @@ void setup() {
   pinMode(17, OUTPUT);
   pinMode(limitSwitchX, INPUT_PULLUP);
   digitalWrite(17, 0);
-
-  x_axis.setCurrentPosition(0);
-  y1_axis.setCurrentPosition(0);
-  y2_axis.setCurrentPosition(0);
-  z_axis.setCurrentPosition(0);
-  
-  x_axis.setMaxSpeed(900);
-  y1_axis.setMaxSpeed(900);
-  y2_axis.setMaxSpeed(900);
-  z_axis.setMaxSpeed(900);
-
-  x_axis.setAcceleration(200);
+  pinMode(13,OUTPUT);
+  x_axis.setMaxSpeed(1000);
+  y1_axis.setMaxSpeed(1000);
+  y2_axis.setMaxSpeed(1000);
+  z_axis.setMaxSpeed(1000);
 
   steppers.addStepper(x_axis);
   steppers.addStepper(y1_axis);
   steppers.addStepper(y2_axis);
   steppers.addStepper(z_axis); 
+
+  pinMode(EM_Button, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(EM_Button), emergencyButtonISR, RISING);
 }
+void emergencyButtonISR() {
+  EM_Button_Pressed = true; // Set the flag
+}
+
 void loop() {
+
+  if(EM_Button_Pressed == 1)
+  {
+    Serial.println("EB");
+    delay(1);
+    while(digitalRead(EM_Button) == 0)
+    {
+      digitalWrite(Buzzor,1);
+      delay(100);
+      digitalWrite(Buzzor,0);
+      delay(100);
+    }
+    EM_Button_Pressed = false;
+  }
   if (Serial.available() > 0) {
     char received = Serial.read();
     inData += received;
@@ -89,9 +99,8 @@ void loop() {
         int Z_pos = inData.indexOf('Z');
         Stp_To_Move[0] = inData.substring(X_pos + 1, Y_pos).toInt();
         Stp_To_Move[1] = inData.substring(Y_pos + 1, Z_pos).toInt();
-        Stp_To_Move[2] = inData.substring(Y_pos + 1, Z_pos).toInt();
+        Stp_To_Move[2] = Stp_To_Move[1];
         Stp_To_Move[3] = inData.substring(Z_pos + 1).toInt();
-        
         if (Stp_To_Move[0] == 0 && Stp_To_Move[1] == 0 && Stp_To_Move[2] == 0 && Stp_To_Move[3] == 0) {
           Serial.println("Ok");
         } else { 
@@ -115,17 +124,22 @@ void loop() {
             break;
         }
       }
+  
       inData = ""; // Clear the input data after processing
-      Reset_All();
-      delayMicroseconds(1);
-    }
+      }
   }
 }
+
 void Move_by_Distance(long int Step[])
 {
+  x_axis.setCurrentPosition(0);
+  y1_axis.setCurrentPosition(0);
+  y2_axis.setCurrentPosition(0);
+  z_axis.setCurrentPosition(0);
   steppers.moveTo(Step);
   steppers.runSpeedToPosition();
 }
+
 void CalibrateX() {
   Serial.println("Calibrating X");
 
@@ -136,17 +150,24 @@ void CalibrateX() {
     delayMicroseconds(400);
     digitalWrite(stepPinX, LOW);
     delayMicroseconds(400);
+    if(EM_Button_Pressed)
+    {
+      return;
+    }
   }
 
   // Double check
   digitalWrite(dirPinX, LOW); 
-  for (int x = 0; x < 2000; x++) { 
+  for (int x = 0; x < 500; x++) { 
     digitalWrite(stepPinX, HIGH);
     delayMicroseconds(400);
     digitalWrite(stepPinX, LOW);
     delayMicroseconds(400);
+    if(EM_Button_Pressed)
+    {
+      return;
+    }
   }
-  
   delay(100);
   digitalWrite(dirPinX, HIGH); 
   while (digitalRead(limitSwitchX) == HIGH) {
@@ -154,11 +175,15 @@ void CalibrateX() {
     delayMicroseconds(400);
     digitalWrite(stepPinX, LOW);
     delayMicroseconds(400);
+    if(EM_Button_Pressed)
+    {
+      return;
+    }
   }
 
   Serial.println("X axis calibrated.");
 
-  long steps_to_move_x = -205 * (400 / 8); 
+  long steps_to_move_x = -320 * (400 / 8); 
   digitalWrite(dirPinX, LOW); 
   
   for (long x = 0; x < abs(steps_to_move_x); x++) {
@@ -166,9 +191,13 @@ void CalibrateX() {
     delayMicroseconds(400);
     digitalWrite(stepPinX, LOW);
     delayMicroseconds(400);
+    if(EM_Button_Pressed)
+    {
+      return;
+    }
   }
 
-  Serial.println("Done X");
+  Serial.println("Ok");
 }
 
 void CalibrateY() {
@@ -178,18 +207,22 @@ void CalibrateY() {
   digitalWrite(dirPinY2, LOW);
 
   while (digitalRead(limitSwitchY1) == HIGH || digitalRead(limitSwitchY2) == HIGH) {
+    if(EM_Button_Pressed)
+    {
+      return;
+    }
     if (digitalRead(limitSwitchY1) == HIGH) {
       digitalWrite(stepPinY1, HIGH);
-      delayMicroseconds(300);
+      delayMicroseconds(200);
       digitalWrite(stepPinY1, LOW);
-      delayMicroseconds(300);
+      delayMicroseconds(200);
+      
     }
-
     if (digitalRead(limitSwitchY2) == HIGH) {
       digitalWrite(stepPinY2, HIGH);
-      delayMicroseconds(300);
+      delayMicroseconds(200);
       digitalWrite(stepPinY2, LOW);
-      delayMicroseconds(300);
+      delayMicroseconds(200);
     }
   }
 
@@ -197,39 +230,42 @@ void CalibrateY() {
   digitalWrite(dirPinY1, HIGH);
   digitalWrite(dirPinY2, HIGH);
 
-  for (int x = 0; x < 2000; x++) {
+  for (int x = 0; x < 1000; x++) {
     digitalWrite(stepPinY1, HIGH);
     digitalWrite(stepPinY2, HIGH);
-    delayMicroseconds(500);
+    delayMicroseconds(300);
     digitalWrite(stepPinY1, LOW);
     digitalWrite(stepPinY2, LOW);
-    delayMicroseconds(500);
+    delayMicroseconds(300);
   }
-
   delay(100);
-
+  
   digitalWrite(dirPinY1, LOW);
   digitalWrite(dirPinY2, LOW);
 
   while (digitalRead(limitSwitchY1) == HIGH || digitalRead(limitSwitchY2) == HIGH) {
+    if(EM_Button_Pressed)
+    {
+      return;
+    }
     if (digitalRead(limitSwitchY1) == HIGH) {
       digitalWrite(stepPinY1, HIGH);
-      delayMicroseconds(500);
+      delayMicroseconds(300);
       digitalWrite(stepPinY1, LOW);
-      delayMicroseconds(500);
+      delayMicroseconds(300);
     }
 
     if (digitalRead(limitSwitchY2) == HIGH) {
       digitalWrite(stepPinY2, HIGH);
-      delayMicroseconds(500);
+      delayMicroseconds(300);
       digitalWrite(stepPinY2, LOW);
-      delayMicroseconds(500);
+      delayMicroseconds(300);
     }
   }
 
   Serial.println("Y1 and Y2 axes calibrated.");
 
-  long steps_to_move_y = 190 * (400 / 8);
+  long steps_to_move_y = 5 * (400 / 8);
 
   digitalWrite(dirPinY1, HIGH);
   digitalWrite(dirPinY2, HIGH);
@@ -237,35 +273,18 @@ void CalibrateY() {
   for (long x = 0; x < abs(steps_to_move_y); x++) {
     digitalWrite(stepPinY1, HIGH);
     digitalWrite(stepPinY2, HIGH);
-    delayMicroseconds(500);
+    delayMicroseconds(300);
     digitalWrite(stepPinY1, LOW);
     digitalWrite(stepPinY2, LOW);
-    delayMicroseconds(500);
+    delayMicroseconds(300);
   }
 
-  Serial.println("Done Y1 and Y2");
+  Serial.println("Ok");
 }
 
 void CalibrateAll() {
-  Serial.println("Starting calibration for all axes");
-
+  Serial.println("Running");
   CalibrateX();
   CalibrateY();
-
-
-  Serial.println("Done All");
-}
-
-void Reset_All()
-{
-  S_Flag = 0;// reset serial Flag
-  D_Flag = 0; // reset direction
-  // Reset Dir array
-  common_Step = 0; // reset commm set
-  High_Step = -1;// reset highstep variable
-  
-  x_axis.setCurrentPosition(0);
-  y1_axis.setCurrentPosition(0);
-  y2_axis.setCurrentPosition(0);
-  z_axis.setCurrentPosition(0);
+  Serial.println("Ok");
 }
